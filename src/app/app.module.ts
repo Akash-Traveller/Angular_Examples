4e9a886548458d75b0ce4cfc845063a427bba6c1
Akash-Traveller <akash.arkvk@gmail.com>
2023-01-09 14:31:35 +0530

    header footer example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildcomComponent } from './childcom/childcom.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildcomComponent
+    ChildcomComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
