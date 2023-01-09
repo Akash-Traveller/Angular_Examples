b0a938ed6ef1b8ca7a15c77fa840b0295257fbc9
Akash-Traveller <akash.arkvk@gmail.com>
2023-01-09 14:10:42 +0530

    commit forloop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,28 @@ export class AppComponent {
   parentMessage : string | undefined;
   show= "blue";
   color="aqua";
+  data=['akash','traveller','m3','demo'];
+  data2 = [{
+    name: 'akash',
+    age: 20,
+    email: 'abx@gmail.com'
+  },
+  {
+    name: 'demo',
+    age:30,
+    email: 'sdffh@gmail.com'
+  },
+  {
+    name: 'amn',
+    age: 10,
+    email: 'sdee@gmail.com'
+  },
+  {
+    name: 'qwe',
+    age: 0,
+    email: 'qqww@gmail.com'
+  }
+]
    constructor() { }
 
   ngOnInit() {
